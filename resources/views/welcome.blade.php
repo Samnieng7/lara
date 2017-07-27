a6d0f1d30653a08497d7ebad58087ddd036f65d2
Samnieng7 <30462983+Samnieng7@users.noreply.github.com>
2017-07-27 11:25:14 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Hello from The World
                 </div>
 
                 <div class="links">
